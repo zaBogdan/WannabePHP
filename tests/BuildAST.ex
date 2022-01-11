5d5fd45f7146749b128ba2e92bb0be2f7dafcfb9
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-01-11 12:07:11 +0200

    finished.

--- tests/BuildAST.ex
+++ tests/BuildAST.ex
@@ -14,4 +14,10 @@ __main__
 
 $x -> (10 + $vec[3]) * 15 + 12.7 + Hello(12);
 
+String $data -> "We are testing funcs";
 Print("Value is", $x);
+
+Int $y -> $x - Max(20, 30) + Min($x, $vec[0]) * Len($data);
+
+Print("Calculating y: ", $y);
+Print("Getting a random val at each run: ", RandomInt());
